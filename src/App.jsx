6a30f15eff0764ae385cac3b499e928bc9a0e801
sumit <mazumdarsumit3@gmail.com>
2024-01-05 23:27:02 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Navbar } from './components';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <h1 className='text-yellow-300 bg-slate-700'>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
