195bf507c6d17fc6a8a85787b6bb719e6e2549b4
sumit <mazumdarsumit3@gmail.com>
2024-03-10 11:12:18 +0530

    fix: changed the layout of the header and footer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,64 @@
 import { Link, NavLink } from 'react-router-dom';
-import logo from '../assets/sm.webp';
+import Logo from '../assets/Logo.svg';
 
 function Navbar() {
   return (
-    <header className='w-full relative md:w-full'>
-      <nav className=' bg-cover-image bg-indigo-300 h-14 flex w-full sm:w-3/4 md:w-full lg:w-full mx-auto fixed'>
+    <header className='w-full relative md:w-full flex flex-row ritems-center justify-center'>
+      <nav className=' bg-header h-20 flex w-full sm:w-3/4 md:w-full lg:w-full mx-auto fixed'>
         <div className='logo sm:w-40'>
           <Link to='/'>
-            <img
-              className='h-9 w-9 mt-2.5 ml-3 lg:h-10 lg:w-11 border-r-1 min-w-5'
-              src={logo}
-              alt='logo'
-            />
+            <img src={Logo} className='h-16 w-16 mt-2' />
           </Link>
         </div>
         <div className='flex items-center lg:w-full sm:w-10'>
           <ul className='flex flex-row mx-11 mt-2 space-x-4 text-blue-800 text-xs lg:text-lg lg:ml-auto lg:mb-5'>
             <li>
-              <NavLink to='/'>Home</NavLink>
+              <NavLink
+                to='/'
+                className={({ isActive }) =>
+                  `${
+                    isActive ? 'text-link' : 'text-black'
+                  } font-handlee font-thin text-sm`
+                }
+              >
+                HOME
+              </NavLink>
             </li>
             <li>
-              <NavLink to='/about'>About</NavLink>{' '}
+              <NavLink
+                to='/projects'
+                className={({ isActive }) =>
+                  `${
+                    isActive ? 'text-link' : 'text-black'
+                  } font-handlee font-thin text-sm`
+                }
+              >
+                PROJECT
+              </NavLink>
             </li>
             <li>
-              <NavLink to='/portfolio'>Portfolio</NavLink>{' '}
+              <NavLink
+                to='/blogs'
+                className={({ isActive }) =>
+                  `${
+                    isActive ? 'text-link' : 'text-black'
+                  } font-handlee font-thin text-sm`
+                }
+              >
+                BLOGS
+              </NavLink>
             </li>
             <li>
-              <NavLink to='/blogs'>Blogs</NavLink>{' '}
-            </li>
-            <li>
-              <NavLink to='/contacts'>Contacts</NavLink>{' '}
+              <NavLink
+                to='/about'
+                className={({ isActive }) =>
+                  `${
+                    isActive ? 'text-link' : 'text-black'
+                  } font-handlee font-thin text-sm`
+                }
+              >
+                ABOUT
+              </NavLink>
             </li>
           </ul>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,15 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      backgroundImage: {},
+      colors: {
+        footer: '#F8F0F4',
+        header: '#DFF6F6',
+        link: '#5200FF',
+      },
+    },
+    fontFamily: {
+      handlee: ['Handlee', 'regular'],
     },
   },
-  plugins: [require('tailwind-scrollbar-hide')],
+  plugins: [],
 };
